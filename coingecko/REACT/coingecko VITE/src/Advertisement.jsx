830b09249732ec79f7c28662695b7fe173aac81a
espelho-07 <darpanparmar1707@gmail.com>
2025-08-14 13:21:12 +0530

    make an advertisement bar

---
+++ coingecko/REACT/coingecko VITE/src/Advertisement.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+function Advertisement() {
+    return (
+        <div className="d-flex container justify-content-center align-items-center mt-2 ad-container-height">
+            <img
+            src=''
+                alt="Advertisement"
+                style={{
+                    height: '100%',
+                    width: 'auto',
+                    maxWidth: '100%',
+                }}
+            />
+        </div>
+    );
+}
+
+export default Advertisement;
